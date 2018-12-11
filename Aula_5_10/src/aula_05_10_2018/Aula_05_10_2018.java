aa2839bd64c64bcf24320c7c503e66ade7c3f8dc
Arthur Dornelas <arthurdornelas99@gmail.com>
2018-12-11 13:23:51 -0200

    Add files via upload

---
+++ Aula_5_10/src/aula_05_10_2018/Aula_05_10_2018.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula_05_10_2018;
+
+/**
+ *
+ * @author ice
+ */
+public class Aula_05_10_2018 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Divisão: ");
+        float res;
+        res = Calculadora.divisao(5, 0);
+        System.out.println("Resultado: "+res);        
+        
+        Calculadora.percorreVetor(0);
+    }
+    
+}
